0a75f0c3374ccc3581426568f8da5bd739b6885a
Ewenlong03 <hwl@beecloud.cn>
2015-03-30 17:55:25 +0800

    update pods

--- ios-pay-demo/Pods/Target Support Files/Pods/Pods-environment.h
+++ ios-pay-demo/Pods/Target Support Files/Pods/Pods-environment.h
@@ -10,23 +10,23 @@
 #define COCOAPODS_POD_AVAILABLE_BeeCloud
 #define COCOAPODS_VERSION_MAJOR_BeeCloud 2
 #define COCOAPODS_VERSION_MINOR_BeeCloud 4
-#define COCOAPODS_VERSION_PATCH_BeeCloud 2
+#define COCOAPODS_VERSION_PATCH_BeeCloud 3
 
 // BeeCloud_UPay
 #define COCOAPODS_POD_AVAILABLE_BeeCloud_UPay
 #define COCOAPODS_VERSION_MAJOR_BeeCloud_UPay 2
 #define COCOAPODS_VERSION_MINOR_BeeCloud_UPay 4
-#define COCOAPODS_VERSION_PATCH_BeeCloud_UPay 2
+#define COCOAPODS_VERSION_PATCH_BeeCloud_UPay 3
 
 // BeeCloud_aliPay
 #define COCOAPODS_POD_AVAILABLE_BeeCloud_aliPay
 #define COCOAPODS_VERSION_MAJOR_BeeCloud_aliPay 2
 #define COCOAPODS_VERSION_MINOR_BeeCloud_aliPay 4
-#define COCOAPODS_VERSION_PATCH_BeeCloud_aliPay 2
+#define COCOAPODS_VERSION_PATCH_BeeCloud_aliPay 3
 
 // BeeCloud_wxPay
 #define COCOAPODS_POD_AVAILABLE_BeeCloud_wxPay
 #define COCOAPODS_VERSION_MAJOR_BeeCloud_wxPay 2
 #define COCOAPODS_VERSION_MINOR_BeeCloud_wxPay 4
-#define COCOAPODS_VERSION_PATCH_BeeCloud_wxPay 2
+#define COCOAPODS_VERSION_PATCH_BeeCloud_wxPay 3
 
